ba17cf941404323c57218244069885c48fb58813
Víctor Ceballos Espinosa <victcebesp@gmail.com>
2018-03-01 17:02:11 +0000

    Green test: two_vertex_graph_two_branches_return_two_step_route

--- test/BranchAndBoundTest.java
+++ test/BranchAndBoundTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -40,5 +41,22 @@ public class BranchAndBoundTest {
         assertEquals(2, route.getCost());
     }
 
+    @Test
+    public void two_vertex_graph_two_branches_return_two_step_route(){
+
+        Edge edge01 = new Edge(0, 1, 3);
+        Edge edge02 = new Edge(0, 1, 2);
+        List<Edge> graph = Arrays.asList(edge01, edge02);
+
+        int maxRouteSize = 2;
+
+        Route route = new Route(maxRouteSize);
+
+        BranchAndBound branchAndBound = new BranchAndBound(graph, maxRouteSize-1);
+        branchAndBound.search_minimum_route(0, 0, 0, route);
+
+        assertArrayEquals(new int []{0, 1}, route.getFinalRoute());
+        assertEquals(2, route.getCost());
+    }
 
 }
