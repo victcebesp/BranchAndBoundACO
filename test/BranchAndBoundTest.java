d7ac7f62181618c213ff47b91bca01711ccb4d33
Víctor Ceballos Espinosa <victcebesp@gmail.com>
2018-03-01 17:27:22 +0000

    Green test: dense_graph_return_four_step_route

--- test/BranchAndBoundTest.java
+++ test/BranchAndBoundTest.java
@@ -79,6 +79,32 @@ public class BranchAndBoundTest {
         assertEquals(4, route.getCost());
     }
 
+    @Test
+    public void dense_graph_return_four_step_route(){
 
+        Edge edge01 = new Edge(0, 1, 1);
+        Edge edge02 = new Edge(0, 2, 3);
+        Edge edge03 = new Edge(0, 3, 2);
+        Edge edge14 = new Edge(1, 4, 5);
+        Edge edge16 = new Edge(1, 6, 3);
+        Edge edge24 = new Edge(2, 4, 4);
+        Edge edge25 = new Edge(2, 5, 3);
+        Edge edge35 = new Edge(3, 5, 2);
+        Edge edge36 = new Edge(3, 6, 7);
+        Edge edge47 = new Edge(4, 7, 4);
+        Edge edge57 = new Edge(5, 7, 1);
+        Edge edge67 = new Edge(6, 7, 1);
+
+        List<Edge> graph = Arrays.asList(edge01, edge02, edge03, edge14, edge16, edge24, edge25, edge35, edge36, edge47, edge57, edge67);
+
+        int maxRouteSize = 4;
+        Route route = new Route(maxRouteSize);
+        BranchAndBound branchAndBound = new BranchAndBound(graph, 7);
+
+        branchAndBound.search_minimum_route(0, 0, 0, route);
+
+        assertArrayEquals(new int[]{0, 1, 6, 7}, route.getFinalRoute());
+        assertEquals(5, route.getCost());
+    }
 
 }
